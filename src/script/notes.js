a791ae2918189832755d0d3e6af3d88f1b006fa0
jackdaniel1996 <46755205+jackdaniel1996@users.noreply.github.com>
2022-06-03 19:17:11 +0200

    Bug fixes v1.2

--- src/script/notes.js
+++ src/script/notes.js
@@ -1,19 +1,11 @@
-//save 2s after Input
-let input = $('.notes-column-head, .notes-entry-head, .notes-entry-text');
-let timer;
-var saving = () => input.on('input', function (e) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-        saveData()
-    }, 1000);
-});
 
-// src for this function https://javascript.plainenglish.io/how-to-build-complex-dom-elements-quickly-b3ead8e09647
-// creating DOM elements
-// NoteToSelf: Attributes (attrs) must be written in camelCase
+
 let dom_utils = {};
+// elements that are editable
+let contentInputs;
 
 // functions to create and append/insert elements
+// NoteToSelf: Attributes (attrs) must be written in camelCase
 function appendDOMobj(type, id, className, innerHTML, attrs, parent){
     let addContent = dom_utils.createEl({
         type: type,
@@ -37,7 +29,8 @@ function insertDOMobj(type, id, className, innerHTML, attrs, parent){
     parent.insertBefore(addContent, parent.lastElementChild);
     return addContent;
 }
-
+// src for this function https://javascript.plainenglish.io/how-to-build-complex-dom-elements-quickly-b3ead8e09647
+// creating DOM elements
 (function(context) {
    
     /**
@@ -77,17 +70,18 @@ function addColButton(){
     appendDOMobj('div', '', 'notes-add-column', 'Add Column +', '', addCol);  
 }
 
-$(document).ready(function(){
-    addColButton();
-});
-
 // add Column
 function addCol(){
     let notesBody = document.getElementById('notesBody');
-    let columnCount = 'column-' + $('.notes-column').length;
+    let columnCount = $('.notes-column').length;
 
     //col body
-    let addColBody = insertDOMobj('div', columnCount, 'notes-column', '', '', notesBody);
+    let addColBody = insertDOMobj('div', 'column-' + columnCount, 'notes-column', '', '', notesBody);
+
+    //settings panel
+    let deleteAttr = {onclick: 'deleteCol(this)'}
+    let addSettingsPanel = appendDOMobj('div', 'settings-' + columnCount, 'notes-col-settings', '', '', addColBody);
+    appendDOMobj('div', '', 'delete-col', '', deleteAttr, addSettingsPanel);
 
     //col content
     let colHeadlineAttr = {contenteditable:''};
@@ -100,14 +94,15 @@ function addCol(){
     addColEntry(addColEntryBtn);
 
     //save
-    saveData();
+    contentInputs = $('.notes-column-head, .notes-entry-head, .notes-entry-text');
+    contentInputs.each(inputSave);
 }
 
 //add Entry
 function addColEntry(elem){
     let colBodyParent = document.getElementById(elem.parentNode.id);
-    let entryCount = 'entry-' + (($('#' + elem.parentNode.id + ' .notes-entry').length) + 1);
-
+    let parentCount = elem.parentNode.id.slice(-1);
+    let entryCount = 'entry-'+ parentCount + '-' + (($('#' + elem.parentNode.id + ' .notes-entry').length) + 1); //id of entry (entry-ColCount-EntryCount)
     //entry body
     let addNotesEntry = insertDOMobj('div', entryCount, 'notes-entry', '', '', colBodyParent);
    
@@ -119,8 +114,35 @@ function addColEntry(elem){
     appendDOMobj('div', '', 'notes-entry-text', 'Entry text', notesAttr, addNotesEntry)
    
     //save
-    input = $('.notes-column-head, .notes-entry-head, .notes-entry-text');
-    saving();
+    contentInputs = $('.notes-column-head, .notes-entry-head, .notes-entry-text');
+    contentInputs.each(inputSave);
+    saveData();
+}
+
+//delete Col
+function deleteCol(col){
+    selectedColumn = col.parentElement.getAttribute('id').slice(-1);
+    $('#column-' + selectedColumn).remove();  
+    renumberContent();
+
+    //save
+    setTimeout(function(){
+        saveData();
+    }, 200); 
+}
+
+function renumberContent(){
+    $('#notesBody .notes-column').each(function(c){
+        c += 1;
+        $(this).attr('id', 'column-' + c);
+        $(this).children('.notes-col-settings').attr('id', 'settings-' + c);
+
+        $(this).children('.notes-entry').each(function(e){
+            e += 1;
+            $(this).attr('id', 'entry-' + c + '-' + e); 
+        });
+    });
+    $('#notesBody > :last-child').attr('id', 'column-0');
 }
 
 // saving data
@@ -128,79 +150,113 @@ var dbContent = {}; // object to store notes content
 
 function saveData(){
     dbContent.columnCount = $('.notes-column').length - 1;
-
-    // get col and headline
-    var i = 0; //column count    
-    do {
-        i += 1;
-        let col = 'column-' + i;
-        let entryCount = document.getElementById(col).querySelectorAll('.notes-entry').length;        
-
-        dbContent[col] = {};        
-        dbContent[col]["headline"] = $('#' + col + ' .notes-column-head').text(); //column headline 
-
-        //get entrys of col and its content
-        var e = 0 //entry count
+    if(dbContent.columnCount > 0)
+    {
+        // get col and headline
+        var i = 0; //column count    
         do {
-            e += 1;
-            let entry = 'entry-' + e;
-            dbContent[col][entry] = {};
-            dbContent[col][entry]["headline"] =  $('#' + col + ' #' + entry + ' .notes-entry-head').text(); // entry headline
-            dbContent[col][entry]["text"] =  $('#' + col + ' #' + entry + ' .notes-entry-text').text(); // entry content
+            i += 1;
+            let col = 'column-' + i;
+            let entryCount = document.getElementById(col).querySelectorAll('.notes-entry').length;
+            dbContent[col] = {};        
+            dbContent[col]["headline"] = $('#' + col + ' .notes-column-head').text(); //column headline 
 
-        }while (e < entryCount)
+            //get entrys of col and its content
+            var e = 0 //entry count
+            do {
+                e += 1;
+                let entry = 'entry-' + i + '-' + e;
+                dbContent[col][entry] = {};
+                dbContent[col][entry]["headline"] =  $('#' + col + ' #' + entry + ' .notes-entry-head').text(); // entry headline
+                dbContent[col][entry]["text"] =  $('#' + col + ' #' + entry + ' .notes-entry-text').text(); // entry content
 
-    } while (i < dbContent.columnCount);
+            }while (e < entryCount)
+            
+        } while (i < dbContent.columnCount);
+        
+        
+        localStorage.setItem('savedData', JSON.stringify(dbContent));        
 
-    console.log('Saved!');
-
-    localStorage.setItem('savedData', JSON.stringify(dbContent));
+        console.log('Saved!');
+    }else{
+        localStorage.setItem('savedData', 0);
+    }
 }
 
 //displaying saved data
-$(document).ready(function(){
+$(document).ready(function(){  
+    addColButton();  
     (function displayData (){
         let savedData = JSON.parse(localStorage.getItem('savedData'));
         let notesBody = document.getElementById('notesBody');
         let notesAttr = {contenteditable:''}
-
-        var c = 0;
+        
         //column
-        do{
-            c += 1;
-            let colId = Object.getOwnPropertyNames(savedData); //TODO: error when saveData does not exist yet. 
-
-            //col body
-            insertDOMobj('div', colId[c], 'notes-column', '', '', notesBody)
-
-            //col headline
-            let col = document.getElementById('column-'+c);
-            
-            appendDOMobj('div', '', 'notes-column-head',  savedData[colId[c]].headline, notesAttr, col)            
-
-            //add entry button
-            let addEntryAttr = {onclick: 'addColEntry(this)'};
-            appendDOMobj('div', '', 'notes-add-entry', 'Add Entry +', addEntryAttr, col);
-            
-            //entrys
-            let entryCount = Object.getOwnPropertyNames(savedData[colId[c]])
-            var e = 0;
+        if(savedData !== null && savedData !== 0)
+        {
+            var c = 0;
             do{
-                e += 1;
-                let entryId = Object.getOwnPropertyNames(savedData[colId[c]]);
-                //entry body
-                insertDOMobj('div', entryId[e], 'notes-entry', '', '', col);
-                
-                //entry content
-                let entry = $('#column-'+c+' #entry-'+e);
-                //entry headline
-                appendDOMobj('div', '', 'notes-entry-head', savedData['column-'+c]['entry-'+e].headline, notesAttr, entry);
-                //entry text
-                appendDOMobj('div', '', 'notes-entry-text', savedData['column-'+c]['entry-'+e].text, notesAttr, entry);
-                
+                c += 1;
+                let colId = Object.getOwnPropertyNames(savedData);
+               
+                //col body
+                insertDOMobj('div', colId[c], 'notes-column', '', '', notesBody)
 
-            }while (e < entryCount.length - 1)
+                //col headline
+                let col = document.getElementById('column-'+c);
 
-        }while (c < savedData.columnCount)
+                //settings panel
+                let deleteAttr = {onclick: 'deleteCol(this)'}
+                let addSettingsPanel = appendDOMobj('div', 'settings-' + c, 'notes-col-settings', '', '', col);
+                appendDOMobj('div', '', 'delete-col', '', deleteAttr, addSettingsPanel);
+                
+                appendDOMobj('div', '', 'notes-column-head',  savedData[colId[c]].headline, notesAttr, col)            
+
+                //add entry button
+                let addEntryAttr = {onclick: 'addColEntry(this)'};
+                appendDOMobj('div', '', 'notes-add-entry', 'Add Entry +', addEntryAttr, col);
+                
+                //entrys
+                let entryCount = Object.getOwnPropertyNames(savedData[colId[c]])
+                var e = 0;
+                do{
+                    e += 1;
+                    let entryId = Object.getOwnPropertyNames(savedData[colId[c]]);
+                    //entry body
+                    insertDOMobj('div', entryId[e], 'notes-entry', '', '', col);
+                    
+                    //entry content
+                    let entry = $('#column-'+c+' #entry-'+c+'-'+e);
+                    //entry headline
+                    appendDOMobj('div', '', 'notes-entry-head', savedData['column-'+c]['entry-'+c+'-'+e].headline, notesAttr, entry);
+                    //entry text
+                    appendDOMobj('div', '', 'notes-entry-text', savedData['column-'+c]['entry-'+c+'-'+e].text, notesAttr, entry);
+                    
+
+                }while (e < entryCount.length - 1)
+
+            }while (c < savedData.columnCount)
+        }
     })();
+
+    contentInputs = $('.notes-column-head, .notes-entry-head, .notes-entry-text');
+    contentInputs.each(inputSave);
 });
+
+//save after input
+let typingTimer;               
+let doneTypingInterval = 500;  
+contentInputs = $('.notes-column-head, .notes-entry-head, .notes-entry-text');
+
+contentInputs.each(inputSave);
+
+function inputSave(input){
+    contentInputs[input].addEventListener('keyup', () => {
+        clearTimeout(typingTimer); 
+        typingTimer = setTimeout(doneTyping, doneTypingInterval);          
+    });
+}
+
+function doneTyping() {
+    saveData()
+}
